1aa9b8ac7e9e039d537d20668dea7d45cd2af90d
emad555 <emadomari40@gmail.com>
2020-07-04 00:59:43 +0400

    iterate the passengers names

--- CW2/cw2/src/cw2/passengerFile.java
+++ CW2/cw2/src/cw2/passengerFile.java
@@ -12,34 +12,44 @@ public class passengerFile {
 	String line = "";
 	String cvsSplitBy = ",";
     List<String> passengers = new ArrayList<>();
-    
+    int counter;
 	bookingDetails pd = new bookingDetails("ss","ss","ss","ss");
-	
+    String[] passengerNames;
+	ArrayList<String> name1 = new ArrayList<String>();
+
 		public void readFile1()
 		{
 			
 		try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-
-        while ((line = br.readLine()) != null) {
+		
+            while ((line = br.readLine()) != null) {
         	
             // use comma as separator
             String[] passengerDetails = line.split(cvsSplitBy);
-       
-          
             bookingDetails pd = new bookingDetails(passengerDetails[0], passengerDetails[1], passengerDetails[2], passengerDetails[3]);
-            String x = passengerDetails[0];
+
+//            ArrayList<String> names = new ArrayList<String>();
+            name1.add(passengerDetails[2]);
             
-            System.out.println(x);
             
+//            
+//			passengerNames[counter]=passengerDetails[2];
+//            
+//            for(counter = 0; counter<passengerDetails[2].length(); counter++ )
+//            System.out.println(passengerNames[counter]);
+//            
             
     	   // list = passengerDetails[2];
           //  System.out.println(list);
 
         }
-
+            
     } catch (IOException e) {
         e.printStackTrace();
     }
+
+        System.out.println(name1.get(3));
+
 }
 }
 	
