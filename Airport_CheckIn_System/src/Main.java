8720d0f2af6b59f9d4ba83434f0f56ef46aaedf7
mohammedsalehhemeda <msalehhemeda@gmail.com>
2020-07-09 14:12:57 +0400

    Updating Main Class

--- Airport_CheckIn_System/src/Main.java
+++ Airport_CheckIn_System/src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-		CheckinManager checkin_manager = new CheckinManager();
-		checkin_manager.run();
-        }
+    	Runnable runnable1 = new Desk1();
+    	Thread thread1 = new Thread(runnable1, "Thread1");
+    	thread1.start();
+        
+    	Runnable runnable2 = new Desk1();
+    	Thread thread2 = new Thread(runnable2, "Thread2");
+    	thread2.start();
+    }
 }
